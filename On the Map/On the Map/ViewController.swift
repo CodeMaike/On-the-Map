7e51916683f2fbb34a382ff948589f206aad9438
Maike Schmidt <maike@simpletechs.net>
2017-03-02 21:04:53 +0100

    new still empty file

---
+++ On the Map/On the Map/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  On the Map
+//
+//  Created by Maike Schmidt on 02/03/2017.
+//  Copyright © 2017 Maike Schmidt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
